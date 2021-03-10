cff6ed770f6e78cc8cdf9e8fd6a8e76d1fc8d9bb
hvitek@seznam.cz <hvitek@seznam.cz>
2021-03-10 16:20:23 +0100

    extract common implementation to parent class allowing to merge static AND file implementaions

---
+++ src/main/java/app/model/simplemodel/staticdata/ModelStaticDataFromEnumImpl.java
@@ -0,0 +1,17 @@
+package app.model.simplemodel.staticdata;
+
+import app.model.simplemodel.suffixesdb.FromPredefinedSuffixesEnumISuffixesDbImpl;
+
+/**
+ * Implementation of simplemodel's data.
+ *
+ * Does not represents any persistence...
+ *
+ * Loads predefined CollectionOfSuffixesCollection from existing ENUM
+ */
+public class ModelStaticDataFromEnumImpl extends AbstractIModelStaticData {
+
+    public ModelStaticDataFromEnumImpl() {
+        super(new FromPredefinedSuffixesEnumISuffixesDbImpl());
+    }
+}
---
+++ src/main/java/app/model/simplemodel/staticdata/ModelStaticDataFromFileImpl.java
@@ -0,0 +1,27 @@
+package app.model.simplemodel.staticdata;
+
+
+import app.model.simplemodel.suffixesdb.FromFileISuffixesDbImpl;
+
+import java.nio.file.Path;
+
+/**
+ * Implementation of simplemodel's data.
+ *
+ * Does represent data persistence using local sqlite database.
+ *
+ * Loads predefined CollectionOfSuffixesCollection from sqlite db.
+ */
+public class ModelStaticDataFromFileImpl extends AbstractIModelStaticData {
+
+    public final static Path DEFAULT_PATH = Path.of("./src/main/resources/app/model/simplemodel/suffixesdb/suffixdb.txt");
+
+    public ModelStaticDataFromFileImpl() {
+        super(new FromFileISuffixesDbImpl(DEFAULT_PATH));
+    }
+
+    public ModelStaticDataFromFileImpl(Path customPathToDb) {
+        super(new FromFileISuffixesDbImpl(customPathToDb));
+    }
+
+}
