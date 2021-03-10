1eea3dfbe7e56d3e1a6e42b45acc78366525456f
hvitek@seznam.cz <hvitek@seznam.cz>
2021-03-10 16:30:00 +0100

    updated sql script that is not used...

--- src/test/resources/model/simplemodel/suffixesdb/create_sqlite_db.sql
+++ src/test/resources/model/simplemodel/suffixesdb/create_sqlite_db.sql
@@ -1,27 +1,29 @@
-CREATE TABLE IF EXISTS suffixes_collection (
+CREATE TABLE IF NOT EXISTS suffixes_collection
+(
     name     TEXT PRIMARY KEY,
     suffixes TEXT NOT NULL
 );
 
-CREATE TABLE IF EXISTS collection_of_suffixes_collections (
-    id INTEGER PRIMARY KEY AUTOINCREMENT
+CREATE TABLE IF NOT EXISTS collection_of_suffixes_collections
+(
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT
 );
 
-CREATE TABLE IF EXISTS suffixes_collections_in_collections (
-    suffixes_id   TEXT    REFERENCES suffixes_collection (name) ON DELETE CASCADE
-                                                                ON UPDATE CASCADE,
+CREATE TABLE IF NOT EXISTS collections_to_suffixes_collections_mapping
+(
+    suffixes_id   TEXT REFERENCES suffixes_collection (name) ON DELETE CASCADE
+        ON UPDATE CASCADE,
     collection_id INTEGER REFERENCES collection_of_suffixes_collections (id) ON DELETE CASCADE
-                                                                             ON UPDATE CASCADE,
+        ON UPDATE CASCADE,
     PRIMARY KEY (
-        suffixes_id,
-        collection_id
-    )
+                 suffixes_id,
+                 collection_id
+        )
 );
 
-CREATE TABLE IF EXISTS current_data (
-    id                  INTEGER PRIMARY KEY AUTOINCREMENT,
-    input_folder        TEXT,
-    output_folder       TEXT,
-    suffixes_collection TEXT    REFERENCES suffixes_collection (name) ON DELETE SET NULL
-                                                                      ON UPDATE CASCADE
+CREATE TABLE IF NOT EXISTS current_data
+(
+    name  TEXT PRIMARY KEY,
+    value TEXT NOT NULL
 );
